37d14b60c645aa1db4e74974ef1d2ae203056313
Marc-Olivier Buob <marc-olivier.buob@lip6.fr>
2013-10-26 13:46:25 +0200

    updated setup.py to remove useless dependancies and updated author email

--- fakeroute/setup.py
+++ fakeroute/setup.py
@@ -8,14 +8,13 @@ setup(name='fakeroute',
     version='0.1',
     description='',
     long_description=long_description,
-    author='TopHat team',
-    author_email='support@top-hat.info',
-    url='http://www.top-hat.info/tools/fakeroute',
+    author='Paris-traceroute team',
+    author_email='paris-traceroute@googlegroups.com',
+    url='https://code.google.com/p/paris-traceroute/wiki/Fakeroute',
     packages=find_packages(),
     install_requires=[
         'dpkt',
-        'nfqueue',
-        'dumbnet'
+        'nfqueue'
     ],
     data_files = [
         ('/usr/share/fakeroute/targets', ['targets/1.1.1.1-asymlb', 'targets/1.1.1.2-lb', 'targets/1.1.1.3-double', 'targets/1.1.1.4-weird']),
